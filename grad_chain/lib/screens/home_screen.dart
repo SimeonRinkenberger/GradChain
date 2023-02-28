c3a202de89231ade66dc19b4fcaf713a9fb8fb62
Jacob Penman <jacobt.penman@calbaptist.edu>
2023-02-28 00:09:38 -0800

    adding the homescreen

---
+++ grad_chain/lib/screens/home_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 23, 67, 24),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 5),
+          Container(
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              //Grad Chain
+              Icon(
+                Icons.circle_sharp,
+                color: Colors.green[200],
+              ),
+              SizedBox(
+                width: 3,
+              ),
+              Text(
+                'GradChain',
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ]),
+          ),
+          SizedBox(height: 30),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            //Block Chain For Deplomas
+            Text(
+              'Blockchain For Deplomas',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ]),
+          SizedBox(height: 40),
+          //Know qualifications with 100% confidence
+          Text(
+            'Know qualifications with 100% confidence',
+            style: TextStyle(fontSize: 15, fontWeight: FontWeight.w100),
+          ),
+          SizedBox(height: 20),
+          //add file
+          SizedBox(height: 40),
+          Container(
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('Log In'),
+              ),
+              SizedBox(width: 30),
+              OutlinedButton(
+                onPressed: () {},
+                child: Text('Sign Up'),
+              ),
+            ]),
+          ),
+        ],
+      ),
+    );
+  }
+}
