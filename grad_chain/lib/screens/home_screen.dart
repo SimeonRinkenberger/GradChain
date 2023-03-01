e7befb1be2da234080343d96fa165688a8400a06
Jacob Penman <jacobt.penman@calbaptist.edu>
2023-02-28 16:09:24 -0800

    responsive

--- grad_chain/lib/screens/home_screen.dart
+++ grad_chain/lib/screens/home_screen.dart
@@ -17,27 +17,30 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          SizedBox(height: 5),
-          Container(
-            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              //Grad Chain
-              Icon(
-                Icons.circle_sharp,
-                color: Colors.green[200],
-              ),
-              SizedBox(
-                width: 3,
-              ),
-              Text(
-                'GradChain',
-                style: TextStyle(
-                  fontSize: 50,
-                  fontWeight: FontWeight.bold,
+          AspectRatio(
+            aspectRatio: 16 / 3,
+            child: Container(
+              child:
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                //Grad Chain
+                Icon(
+                  Icons.circle_sharp,
+                  color: Colors.green[200],
                 ),
-              ),
-            ]),
+                SizedBox(
+                  width: 3,
+                ),
+                Text(
+                  'GradChain',
+                  style: TextStyle(
+                    fontSize: 70,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ]),
+            ),
           ),
-          SizedBox(height: 30),
+          SizedBox(height: 5),
           Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
             //Block Chain For Deplomas
             Text(
@@ -48,39 +51,43 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
           ]),
-          SizedBox(height: 40),
+          SizedBox(height: 10),
           //Know qualifications with 100% confidence
           Text(
             'Know qualifications with 100% confidence',
             style: TextStyle(fontSize: 15, fontWeight: FontWeight.w100),
           ),
-          SizedBox(height: 20),
+          SizedBox(height: 10),
           //add file
-          SizedBox(height: 40),
+          SizedBox(height: 10),
           Container(
-            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              //login
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => LoginScreen()),
-                  );
-                },
-                child: Text('Log In'),
-              ),
-              SizedBox(width: 30),
-              //sign up
-              OutlinedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SignupScreen()),
-                  );
-                },
-                child: Text('Sign Up'),
-              ),
-            ]),
+            child: AspectRatio(
+              aspectRatio: 16 / 3,
+              child:
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                //login
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()),
+                    );
+                  },
+                  child: Text('Log In'),
+                ),
+                SizedBox(width: 30),
+                //sign up
+                OutlinedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignupScreen()),
+                    );
+                  },
+                  child: Text('Sign Up'),
+                ),
+              ]),
+            ),
           ),
         ],
       ),
