dd63f07906ff7ebc619d34b540f7ac5bb5d1f8cb
Patrick Calderon <patriciocc98@gmail.com>
2023-03-28 14:00:06 -0700

    Modified add diploma screen

--- grad_chain/lib/screens/university/add_diploma_screen.dart
+++ grad_chain/lib/screens/university/add_diploma_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:grad_chain/widgets/constrants.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 
@@ -9,6 +10,9 @@ import 'package:grad_chain/providers/user_provider.dart';
 import 'package:grad_chain/resources/firestore_methods.dart';
 import 'package:grad_chain/utils/colors.dart';
 import 'package:grad_chain/utils/utils.dart';
+
+import '../../resources/auth_methods.dart';
+import '../../widgets/text_field_input.dart';
 
 class AddDiplomaScreen extends StatefulWidget {
   const AddDiplomaScreen({super.key});
@@ -20,6 +24,13 @@ class AddDiplomaScreen extends StatefulWidget {
 class _AddDiplomaScreenState extends State<AddDiplomaScreen> {
   Uint8List? _file;
   final TextEditingController _descriptionController = TextEditingController();
+
+  // CREATE STUDENT CONTROLLERS
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _bioController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  Uint8List? _image;
   bool _isLoading = false;
 
   void postImage(
@@ -102,12 +113,46 @@ class _AddDiplomaScreenState extends State<AddDiplomaScreen> {
     });
   }
 
+  void signUpUser() async {
+    setState(() {
+      _isLoading = true;
+    });
+    String res = await AuthMethods().signUpUser(
+      email: _emailController.text,
+      password: _passwordController.text,
+      username: _usernameController.text,
+      bio: _bioController.text,
+      file: _image!,
+    );
+
+    setState(() {
+      _isLoading = false;
+    });
+
+    if (res != 'success') {
+      showSnackBar(res, context);
+    } else {}
+
+    print(res);
+  }
+
+  void selectImage() async {
+    Uint8List im = await pickImage(ImageSource.gallery);
+    setState(() {
+      _image = im;
+    });
+  }
+
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
     // ADD ALL CONTROLLERS
     _descriptionController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _bioController.dispose();
+    _usernameController.dispose();
   }
 
   @override
@@ -127,12 +172,12 @@ class _AddDiplomaScreenState extends State<AddDiplomaScreen> {
           )
         : Scaffold(
             appBar: AppBar(
-              backgroundColor: mobileBackgroundColor,
+              backgroundColor: defaultBackgroundColor,
               leading: IconButton(
                 icon: const Icon(Icons.arrow_back),
                 onPressed: clearImage,
               ),
-              title: const Text('Post to'),
+              title: const Text('Upload diploma for'),
               centerTitle: false,
               actions: [
                 TextButton(
@@ -142,7 +187,7 @@ class _AddDiplomaScreenState extends State<AddDiplomaScreen> {
                     user.photoUrl,
                   ),
                   child: const Text(
-                    'Post',
+                    'Upload',
                     style: TextStyle(
                       color: Colors.blueAccent,
                       fontWeight: FontWeight.bold,
@@ -160,6 +205,221 @@ class _AddDiplomaScreenState extends State<AddDiplomaScreen> {
                         padding: EdgeInsets.only(top: 0),
                       ),
                 const Divider(),
+
+                /* // START OF SIGNUP FORM
+
+                // Row(
+                //   children: [
+                //     // Flexible(
+                //     //   child: Container(),
+                //     //   flex: 2,
+                //     // ),
+                //     // // svg image (we use a package called flutter_svg to show svg images)
+                //     // const Icon(
+                //     //   Icons.lock,
+                //     //   size: 100,
+                //     // ),
+                //     // const SizedBox(height: 50),
+
+                //     Text(
+                //       'Create the student',
+                //       style: TextStyle(
+                //         color: Colors.grey[700],
+                //         fontSize: 42,
+                //       ),
+                //     ),
+                //     const SizedBox(height: 64),
+                //     Stack(
+                //       children: [
+                //         _image != null
+                //             ? CircleAvatar(
+                //                 radius: 64,
+                //                 backgroundImage: MemoryImage(_image!),
+                //               )
+                //             : const CircleAvatar(
+                //                 radius: 64,
+                //                 backgroundImage: NetworkImage(
+                //                   'https://t3.ftcdn.net/jpg/00/64/67/80/360_F_64678017_zUpiZFjj04cnLri7oADnyMH0XBYyQghG.jpg',
+                //                 ),
+                //               ),
+                //         Positioned(
+                //           bottom: -10,
+                //           left: 80,
+                //           child: IconButton(
+                //             onPressed: selectImage,
+                //             icon: const Icon(
+                //               Icons.add_a_photo,
+                //             ),
+                //           ),
+                //         )
+                //       ],
+                //     ),
+                //     const SizedBox(
+                //       height: 24,
+                //     ),
+                //     // text field input for username
+                //     TextFieldInput(
+                //       hintText: 'Enter your username',
+                //       textInputType: TextInputType.text,
+                //       textEditingController: _usernameController,
+                //     ),
+                //     const SizedBox(
+                //       height: 24,
+                //     ),
+                //     // text input for email
+                //     TextFieldInput(
+                //       hintText: 'Enter your email',
+                //       textInputType: TextInputType.emailAddress,
+                //       textEditingController: _emailController,
+                //     ),
+                //     const SizedBox(
+                //       height: 24,
+                //     ),
+                //     // text input for password
+                //     TextFieldInput(
+                //       hintText: 'Enter your password',
+                //       textInputType: TextInputType.text,
+                //       textEditingController: _passwordController,
+                //       isPass: true,
+                //     ),
+                //     const SizedBox(
+                //       height: 24,
+                //     ),
+                //     // text field for bio
+                //     TextFieldInput(
+                //       hintText: 'Enter your bio',
+                //       textInputType: TextInputType.text,
+                //       textEditingController: _bioController,
+                //     ),
+                //     const SizedBox(
+                //       height: 24,
+                //     ),
+                //     // button for login
+                //     InkWell(
+                //       onTap: signUpUser,
+                //       child: Container(
+                //         child: _isLoading
+                //             ? const Center(
+                //                 child: CircularProgressIndicator(
+                //                   color: primaryColor,
+                //                 ),
+                //               )
+                //             : const Text(
+                //                 'Sign up',
+                //                 style: TextStyle(
+                //                   color: Colors.white,
+                //                   fontWeight: FontWeight.bold,
+                //                   fontSize: 16,
+                //                 ),
+                //               ),
+                //         width: double.infinity,
+                //         alignment: Alignment.center,
+                //         padding: const EdgeInsets.symmetric(vertical: 12),
+                //         decoration: const ShapeDecoration(
+                //           shape: RoundedRectangleBorder(
+                //             borderRadius: BorderRadius.all(
+                //               Radius.circular(4),
+                //             ),
+                //           ),
+                //           color: Colors.black,
+                //         ),
+                //       ),
+                //     ),
+
+                //     const SizedBox(
+                //       height: 12,
+                //     ),
+                //     Flexible(
+                //       child: Container(),
+                //       flex: 2,
+                //     ),
+                //     // transitioning to sign up
+                //     Row(
+                //       mainAxisAlignment: MainAxisAlignment.center,
+                //       children: [
+                //         Container(
+                //           padding: const EdgeInsets.symmetric(
+                //             vertical: 12,
+                //           ),
+                //           child: const Text("Already have an account?"),
+                //         ),
+                //         GestureDetector(
+                //           onTap: () {},
+                //           child: Container(
+                //             padding: const EdgeInsets.symmetric(
+                //               vertical: 12,
+                //               horizontal: 4,
+                //             ),
+                //             child: const Text(
+                //               "Log in",
+                //               style: TextStyle(
+                //                 fontWeight: FontWeight.bold,
+                //                 color: Colors.lightBlueAccent,
+                //               ),
+                //             ),
+                //           ),
+                //         ),
+                //       ],
+                //     )
+                //   ],
+                // ),
+
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+                //   crossAxisAlignment: CrossAxisAlignment.start,
+                //   children: [
+                //     Text(
+                //       'Create the student',
+                //       style: TextStyle(
+                //         color: Colors.grey[700],
+                //         fontSize: 42,
+                //       ),
+                //     ),
+                //     const Divider(),
+                //   ],
+                // ),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+                //   crossAxisAlignment: CrossAxisAlignment.start,
+                //   children: [
+                //     const Divider(),
+                //   ],
+                // ),
+                // Row(
+                //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+                //   crossAxisAlignment: CrossAxisAlignment.start,
+                //   children: [
+                //     Stack(
+                //       children: [
+                //         _image != null
+                //             ? CircleAvatar(
+                //                 radius: 64,
+                //                 backgroundImage: MemoryImage(_image!),
+                //               )
+                //             : const CircleAvatar(
+                //                 radius: 64,
+                //                 backgroundImage: NetworkImage(
+                //                   'https://t3.ftcdn.net/jpg/00/64/67/80/360_F_64678017_zUpiZFjj04cnLri7oADnyMH0XBYyQghG.jpg',
+                //                 ),
+                //               ),
+                //         Positioned(
+                //           bottom: -10,
+                //           left: 80,
+                //           child: IconButton(
+                //             onPressed: selectImage,
+                //             icon: const Icon(
+                //               Icons.add_a_photo,
+                //             ),
+                //           ),
+                //         )
+                //       ],
+                //     ),
+                //     const Divider(),
+                //   ],
+                // ),
+                */ // END OF SIGN UP FORM
+
+                // START OF THE PICTURE UPLOAD FORM
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -181,7 +441,7 @@ class _AddDiplomaScreenState extends State<AddDiplomaScreen> {
                       ),
                     ),
 
-                    // IMAGE UPLOADED
+                    // THIS SHOWS THE IMAGE THAT HAS BEEN JUST SELECTED
                     SizedBox(
                       height: 45,
                       width: 45,
