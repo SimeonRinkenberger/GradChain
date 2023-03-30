a1bf90dace316a7f27141b375975422267b4202e
Simeon Rinkenberger <simeonscott.rinkenberger@calbaptist.edu>
2023-03-30 10:49:24 -0700

    ipfs_upload actualy updated this time

--- blockchain/ipfs_upload.py
+++ blockchain/ipfs_upload.py
@@ -1,23 +1,25 @@
 import requests
 import sys
 import base64
-import os
 
 INFURA_PROJECT_ID = "2Nia36psZhuJRKW5X9AxNzIOQ0q"
 INFURA_PROJECT_SECRET = "204dc57525ec7ff705eae55165772478"
 INFURA_API_URL = f"https://ipfs.infura.io:5001/api/v0/add?project_id={INFURA_PROJECT_ID}"
 
-def upload_to_ipfs(file_path):
+def upload_to_ipfs():
+    url = "https://firebasestorage.googleapis.com/v0/b/gradchain-55ffd.appspot.com/o/diplomas%2FFRMYr9ryoZZVkl8ADJTmfbX2hh13?alt=media&token=7486de5b-1c59-4f30-b436-61341d7c7b67"
+    file = requests.get(url, allow_redirects=True)
+
     try:
         # Prepare the authentication header
         auth_header = f"Basic {base64.b64encode(f'{INFURA_PROJECT_ID}:{INFURA_PROJECT_SECRET}'.encode()).decode()}"
 
         # Read the file as binary data
-        with open(file_path, 'rb') as file:
-            file_data = file.read()
+        # with open(file_path, 'rb') as file:
+        #     file_data = file.read()
 
         # Send the request to upload the file to IPFS
-        response = requests.post(INFURA_API_URL, headers={'Authorization': auth_header}, files={'file': file_data})
+        response = requests.post(INFURA_API_URL, headers={'Authorization': auth_header}, files={'file': file.content})
 
         # Check if the request was successful
         if response.status_code == 200:
@@ -25,10 +27,10 @@ def upload_to_ipfs(file_path):
 
             # Get the IPFS hash (CID) and file size
             ipfs_hash = result['Hash']
-            file_size = os.path.getsize(file_path)
+            # file_size = os.path.getsize(file_path)
 
             print(f"Uploaded file to IPFS with CID: {ipfs_hash}")
-            print(f"File size: {file_size} bytes")
+            # print(f"File size: {file_size} bytes")
 
             return ipfs_hash
         else:
@@ -39,9 +41,9 @@ def upload_to_ipfs(file_path):
         sys.exit(1)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python ipfs_upload.py <file_path>")
-        sys.exit(1)
-
-    file_path = sys.argv[1]
-    upload_to_ipfs(file_path)
+    # if len(sys.argv) != 2:
+    #     print("Usage: python ipfs_upload.py <file_path>")
+    #     sys.exit(1)
+    #
+    # file_path = sys.argv[1]
+    upload_to_ipfs()
