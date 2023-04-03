3c9a5ca829dcb14975e7e6ae1b373b7100e9475f
Simeon Rinkenberger <simeonscott.rinkenberger@calbaptist.edu>
2023-04-02 22:59:30 -0700

    ipfs_upload method updated to match google cloud function code

--- blockchain/ipfs_upload.py
+++ blockchain/ipfs_upload.py
@@ -1,14 +1,16 @@
-import requests
 import base64
+import requests
 
 INFURA_PROJECT_ID = "2Nia36psZhuJRKW5X9AxNzIOQ0q"
 INFURA_PROJECT_SECRET = "204dc57525ec7ff705eae55165772478"
 INFURA_API_URL = f"https://ipfs.infura.io:5001/api/v0/add?project_id={INFURA_PROJECT_ID}"
 
-url = "https://firebasestorage.googleapis.com/v0/b/gradchain-55ffd.appspot.com/o/diplomas%2FFRMYr9ryoZZVkl8ADJTmfbX2hh13?alt=media&token=7486de5b-1c59-4f30-b436-61341d7c7b67"
-file = requests.get(url, allow_redirects=True)
+def ipfs_upload(flutter_request):
+  flutter_request_json = flutter_request.get_json()
+  url = flutter_request_json['url']
+  file = requests.get(url, stream=True)
 
-try:
+  try:
     # Prepare the authentication header
     auth_header = f"Basic {base64.b64encode(f'{INFURA_PROJECT_ID}:{INFURA_PROJECT_SECRET}'.encode()).decode()}"
 
@@ -17,15 +19,13 @@ try:
 
     # Check if the request was successful
     if response.status_code == 200:
-        result = response.json()
+      result = response.json()
 
-        # Get the IPFS hash (CID) and file size
-        ipfs_hash = result['Hash']
+      # Get the IPFS hash (CID)
+      ipfs_hash = result['Hash']
 
-        print(f"Uploaded file to IPFS with CID: {ipfs_hash}")
-        print(f"file can be found here: https://ipfs.io/ipfs/{ipfs_hash}")
-
+      return f'https://ipfs.io/ipfs/{ipfs_hash}'
     else:
-        print(f"Error uploading file to IPFS: {response.status_code} {response.text}")
-except Exception as e:
-    print(f"Error uploading file to IPFS: {e}")
+      return "error"
+  except Exception as e:
+    return e
