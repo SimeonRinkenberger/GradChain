c9581704e056ddca4ae0de6c634c43d3cb601cec
Jered Napalan <jered.napalan@icloud.com>
2023-04-05 23:31:21 -0700

    Removed hard coded value for Uni View Docs button diploma

--- grad_chain/lib/widgets/list_widget_card.dart
+++ grad_chain/lib/widgets/list_widget_card.dart
@@ -15,9 +15,10 @@ import '../models/student.dart';
 
 class ListWidgetCard extends StatefulWidget {
   final snap;
-  //final snap2;
-
-  const ListWidgetCard({Key? key, required this.snap}) : super(key: key);
+  const ListWidgetCard({
+    Key? key,
+    required this.snap,
+  }) : super(key: key);
 
   @override
   State<ListWidgetCard> createState() => _ListWidgetCardState();
@@ -29,8 +30,6 @@ class _ListWidgetCardState extends State<ListWidgetCard> {
     final model.User? user = Provider.of<UserProvider>(context).getUser;
     final Student? stu = Provider.of<UserProvider>(context).getStu;
     String studentPhoto = widget.snap[user != null ? 'photoUrl' : 'diplomaUrl'];
-    String placeHolderPhoto =
-        "https://cdn.shopify.com/s/files/1/1699/4809/products/SD_Penn_Foster_High_School_Diploma_Converted_-_Copy_1024x1024@2x.jpg?v=1556304897";
 
     final width = MediaQuery.of(context).size.width;
 
@@ -45,10 +44,13 @@ class _ListWidgetCardState extends State<ListWidgetCard> {
               Navigator.of(context).pushReplacement(
                 MaterialPageRoute(
                   builder: (context) => ViewDocument(
-                      stuPhoto: user != null ? placeHolderPhoto : studentPhoto,
-                      user: user),
+                      stuPhoto: studentPhoto,
+                      user: user //Needed for back button to work properly
+                      ),
                 ),
               );
+
+              ;
             },
             child: Text('View Documents')));
   }
