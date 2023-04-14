989a2be0626ec0799413f94628c68f814b3678c3
Simeon Rinkenberger <simeonscott.rinkenberger@calbaptist.edu>
2023-04-14 00:22:56 -0700

    json access key added to cloud function to hopefully bypass CORS

--- blockchain/ipfs_upload.py
+++ blockchain/ipfs_upload.py
@@ -7,16 +7,7 @@ from google.oauth2 import service_account
 
 json_key_content = '''
 {
-  "type": "service_account",
-  "project_id": "gradchain-55ffd",
-  "private_key_id": "ff8be08653a846353b4f47e84013d551dc655164",
-  "private_key": "-----BEGIN PRIVATE KEY-----\\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDGzXe+G7kWyFsh\\nM7qsllnbIPpCQN0S38KoPbiOPIMNhKaXgAToQjrreIywi+A3s9zP7CLmYmNkfTf2\\nkgoB/LR69uulMot0jNrilp8q9r3FI7p18DjBDijhqhDt54UiIQyitioq7OrQTbQY\\nV0LZ0TICn8z6Rl3j4IBx0AaTUDOpQUMYzUSnWPexSDy44Bd8YRWZaaJCpnnTVAGs\\nLPgbiS09QuMGQaXkKADDMITfLNfLSIr4ZwXY2GUztQunT0Ko3nE/aPwSqSLijF0J\\nWWIf2nj0rbtu3h38xzmR48OClsKn6o80noyXkKeNi8WqbLkD+c9y4zA6g+lfJvyq\\n/s5fxqAjAgMBAAECggEARjdxKSzqiVD32vjF4Mpz71EtAzScwNtWigzOwDJASYiG\\nPLFjSlOGMwAPlRW6eoIlJi0twHiUyXvuxcCcF0qVxm6rGVyKY/6SGwD/M/IVYel2\\nJMzVht6E8vuZ4iVpYP4SU/AKYcHvR+aCCyza+EsKomGlyOoMpH15cHl8sRBdRtSd\\nZ6/TayNJ586K0hLYwfnW7I7Dcu1lW38v5zeWNlGTLO1oZsCJLgNxow8/FEuRkWjm\\nfqy0J01tj0Jd88hccTEmdBlJFVRb7Rr60M6cag32XwepTyH4ywru3s3M27Tv1uKJ\\ndPB7Cp++pIEM43apALAhS8QJAZfW6B+rnOkK4Wh44QKBgQD60hxOq8/ipJm8EfQq\\nd7HYW6QwzNh3b2Blv/1AF6xCDaP+r4HUX57pnZxbKXHB+tNg/UYYOr4YdscLO4XW\\nWOciFIlqEFfYZQGS0fjqxR4/p3lkRmhzzHM7OJ10nLOijPDGTwjhJQ9kd+U7yooe\\nINEr61oyBywzGewfFiWySUdFwwKBgQDK6GD0iL8c7EAOKxHfdhfblkMI8K7hEylQ\\nridaYuZRwHuUsSlWhNJaO9ZaQVEXpoD3LaQHjr7mZCfj60WXXGVa9EOsaZBxh+aB\\n3gtgoBRpxobj8qhUSKxQ1V2xFZEoR8M4CLoYLBMmjzFCWkCpN3Punn8dI1uZcyMa\\nARG28Cq2IQKBgQDCL7eQxP4+tzb631+NHXxISLL2wTF6TnVwcVKts2Y/AXaNMO4w\\nMRPERK0P+ydCimHqvkvtaFVSdWsm7B9zUwMnZIT648pPE+xC8mNnx9Lib1PRaZ1m\\nwcwY9n0ZElUVLU8gCYKTZPDk1NpaDzYdm0S2XxFGmD0FFzWy35+xx3/xpwKBgA78\\nKmzkzHhuWiyxsZg3OHvXFQxL+h5VM+/pbK6YLzpKZNYOX2csV/yh/qqbuElu2odm\\nr35/ZEnPi8KhVXpgMHwPHhBVm31G9BcTNvPj3p4RM1USTqwq0c8GcuzpEtdOtXQl\\n8NhoKOHoIVMtJylYFw/AVNLqEZG7lxQMkCpagk0BAoGAcLstFOKijf87zqno346s\\nVXXlsabOlFox5ScR0f+ULX7ikDvDF4iBKk3OSj/FN1P1PARo+O9mteljQ8f24p9g\\nLFTJOuIFgd2WlWNtFjd7aI8Kjvwj83lOImOzy/+dV6nce3ClKg41oMcck6X56EHn\\nSgJb3WoDTGyuaTiHnDsknHI=\\n-----END PRIVATE KEY-----\\n",
-  "client_email": "storage-viewer@gradchain-55ffd.iam.gserviceaccount.com",
-  "client_id": "116802014986302264716",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/storage-viewer%40gradchain-55ffd.iam.gserviceaccount.com"
+  # this content is on the google cloudconsole but not here to keep the access keys private
 }
 '''
 
